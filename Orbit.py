b5d0475b59ba23c95322c5aa6592b7c49a180a1e
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2022-10-15 17:13:39 +0800

    modify

--- Orbit.py
+++ Orbit.py
@@ -4,6 +4,7 @@ from math import pi
 
 altitude: float = 600e3
 position_0 = np.array([[R + altitude, 0, 0]], dtype=float).T
+position_1 = np.array([[R + 700e3, 0, 0]], dtype=float).T
 
 orbit_inclination: float = 54.24 * (pi/180)
 semi_major: float = np.linalg.norm(position_0)
@@ -18,3 +19,21 @@ attitude_0 = np.array([[1, 0, 0, 0]], dtype=float).T
 omega_0 = np.array([[0.08, -0.02, 0.03]], dtype=float).T
 
 BB = np.array([[0, 0, 0]], dtype=float).T
+
+def semi_major(position: float):
+    return np.linalg.norm(position)
+
+def circular_vel(semi_major: float):
+    return np.sqrt(mu/semi_major)
+
+def period(semi_major: float):
+    return 2*pi/np.sqrt(mu)*semi_major**(3/2)
+
+def vel_y(circular_vel: float, orbit_inclination: float):
+    return circular_vel * np.cos(orbit_inclination)
+
+def vel_z(circular_vel: float, orbit_inclination: float):
+    return circular_vel * np.sin(orbit_inclination)
+
+def velocity(circular_vel: float, orbit_inclination: float):
+    return np.array([[0, (circular_vel * np.cos(orbit_inclination)), (circular_vel * np.sin(orbit_inclination))]], dtype=float).T

c1566618ed2831dd2aeed6873a6d11b37000adfd
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2022-11-03 02:39:35 +0800

    remove useless code

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ orbit_list.append(Orbit(position = [[R + 600e3, 0, 0]], orbit_inclination = 54.2
 print("End of Simulation")
 
 data = np.array(orbit_list[0].data)
-print(np.array(orbit_list[0].time))
 # data = np.concatenate((data, probe.get()), axis=1)
 
-np.savetxt("simulation.csv", data, delimiter=',')
+# np.savetxt("simulation.csv", data, delimiter=',')
 
 # Viz.View(data, orbit_list[0].time)
